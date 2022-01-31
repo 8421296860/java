09b1e6f18e36b65994606e4a70a269b46f05f716
BHUSHAN SATISH DESHMUKH <78414877+8421296860@users.noreply.github.com>
2022-01-31 13:43:31 +0530

    #1

---
+++ Dog.java
@@ -0,0 +1,20 @@
+public class Dog{
+
+public static void main(String[] args){
+
+String name="Bhushan";
+int age=21;
+System.out.print("name of coder "+name+" age of coder "+age);
+while(age>18){
+System.out.print("you are elogible for voting");
+if(name=="Bhushan"){
+age=age-1;
+}
+
+
+}
+
+}
+
+
+}
